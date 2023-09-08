218b88c720f108d77057e253892636d5910c7c11
Christkorf <christianaarkorful28@gmail.com>
2023-09-08 09:31:01 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: 0
+*/
+
+int main(void)
+{
+	char c;
+
+	char R;
+
+	c = 'a';
+	R = 'A';
+	while
+		(c <= 'z') {
+		putchar (c);
+		c++;
+		}
+	while
+		(R <= 'Z') {
+		putchar (R);
+		R++;
+		}
+	putchar('\n');
+	return (0);
+}
